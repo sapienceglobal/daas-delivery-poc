3c7e48b063a4ea3d16d9dd719e763107b877261e
sapience <sapienceglobal@example.com>
2026-07-29 17:06:47 +0530

    imp

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -19,6 +19,10 @@ const nextConfig = {
         source: '/api/:path*',
         destination: 'http://127.0.0.1:5001/api/:path*',
       },
+      {
+        source: '/socket.io/:path*',
+        destination: 'http://127.0.0.1:5001/socket.io/:path*',
+      },
     ];
   },
 }
