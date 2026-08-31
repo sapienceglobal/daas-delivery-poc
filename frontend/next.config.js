d5fc8af6abdb17dd20e02f4e3729a58df0885f6e
sapience <sapienceglobal@example.com>
2026-08-31 12:44:52 +0530

    increase size of image upload in next config file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -17,7 +17,6 @@ const nextConfig = {
     serverActions: {
       bodySizeLimit: '50mb',
     },
-    middlewareClientMaxBodySize: '50mb',
     proxyClientMaxBodySize: '50mb',
   },
   async rewrites() {
